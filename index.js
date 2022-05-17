0b7912319884267cd6921baf5579fe1761f6e816
Super <38338700+superpowers04@users.noreply.github.com>
2022-05-16 20:14:46 -0400

    Use VXTwitter instead, as it actually exists

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const { Plugin } = require('powercord/entities');
 const { inject, uninject } = require('powercord/injector');
 const { messages } = require('powercord/webpack');
 
-module.exports = class fxtwitterauto extends Plugin {
+module.exports = class vxtwitterauto extends Plugin {
 	startPlugin() {
-		inject('fxtwitterauto', messages, 'sendMessage', args => {
-			if (args[1].content.search(/https?:\/\/twitter/g) !== -1) args[1].content = args[1].content.replace(/https?:\/\/twitter/g,"https://fxtwitter");
+		inject('vxtwitterauto', messages, 'sendMessage', args => {
+			if (args[1].content.search(/https?:\/\/twitter/g) !== -1) args[1].content = args[1].content.replace(/https?:\/\/twitter/g,"https://vxtwitter");
 			return args;
 		}, true);
 	}
 	pluginWillUnload() {
-		uninject('fxtwitterauto');
+		uninject('vxtwitterauto');
 	}
 };
